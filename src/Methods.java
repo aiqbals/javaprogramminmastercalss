879b9e4a3e823bdabb54a147bc250d8e9f6e6e2f
Asif Hoque <asif.engr13@gmail.com>
2021-06-02 23:49:53 +0200

    operators

--- src/Methods.java
+++ src/Methods.java
@@ -23,7 +23,30 @@ public class Methods {
 //			System.out.println(finalScore);
 //		}
 		//calculateScore(gameOver, score, levelCompleted, bonus);
-		calculateScore(false, 10000, 8, 200 );
+		//calculateScore(false, 10000, 8, 200 );
+		highScore = calculateScore(false, 10000, 8, 200 );
+		System.out.println(highScore);
+		
+		
+		
+		int highScorePos = calculateHighScorePosition(1500);
+		// return value must be stored into a variable
+		displayHighScorePosition("Asif", highScorePos);
+		
+		highScorePos = calculateHighScorePosition(900);
+		displayHighScorePosition("Aydin", highScorePos);
+		
+		highScorePos = calculateHighScorePosition(400);
+		displayHighScorePosition("Lany", highScorePos);
+		
+		highScorePos = calculateHighScorePosition(50);
+		displayHighScorePosition("Zeynap", highScorePos);
+		
+		// What if the score is exact the base case e.g., 1000 or 500 - logic hole
+		// Result: use >= instead of =
+		highScorePos = calculateHighScorePosition(1000);
+		displayHighScorePosition("Zeynap", highScorePos);
+		
 	}
 	
 	// To avoid duplication and other cons, to clean up extra code, we can use method
@@ -46,5 +69,20 @@ public class Methods {
 		
 		return -1; // -1 can be used as err or value not found or invalid value
 		// else can be used as well
+	}
+	
+	public static void displayHighScorePosition(String name, int position) {
+		System.out.println(name + " managed to get into position " + position + " on the high score table");
+	}
+	public static int calculateHighScorePosition(int score) {
+		if(score >= 1000) {
+			return 1;
+		} else if(score >= 500 && score < 1000) {
+			return 2;
+		} else if(score >= 100 && score < 500) {
+			return 3;
+		} else {
+			return 4;
+		}
 	}
 }
