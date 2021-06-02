4da1749bccd51c0d3bf06c6654aaa6f5dbf5a055
Asif Hoque <asif.engr13@gmail.com>
2021-06-02 19:20:26 +0200

    operators

--- src/Operators.java
+++ src/Operators.java
@@ -65,6 +65,8 @@ public class Operators {
 		
 		// All operators java
 		// https://docs.oracle.com/javase/tutorial/java/nutsandbolts/opsummary.html
-		// http://www.cs.bilkent.edu.tr/~guvenir/courses/CS101/op_precedence.html
+		
+		// Java precedence - parenthesis gets higher prece, thn multi, divid, moduls and thn add substract
+		// http://www.cs.bilkent.edu.tr/~guvenir/courses/CS101/op_precedence.html		
 	}
 }
