eb6fb97138706e81289fb74880a0d6329446c9c5
Asif Hoque <asif.engr13@gmail.com>
2021-06-02 23:21:46 +0200

    operators

--- src/Methods.java
+++ src/Methods.java
@@ -10,6 +10,7 @@ public class Methods {
 //			int finalScore = score + (levelCompleted * bonus);
 //			System.out.println(finalScore);
 //		}
+		//calculateScore(gameOver, score, levelCompleted, bonus);
 		calculateScore(true, 800, 5, 100 );
 		
 //		score = 10000;
@@ -19,15 +20,29 @@ public class Methods {
 //			int finalScore = score + (levelCompleted * bonus);
 //			System.out.println(finalScore);
 //		}
-		calculateScore(true, 10000, 8, 200 );
+		//calculateScore(gameOver, score, levelCompleted, bonus);
+		calculateScore(false, 10000, 8, 200 );
 	}
 	
 	// To avoid duplication and other cons, to clean up extra code, we can use method
-	public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+//	public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+//		if(gameOver == true) {
+//			int finalScore = score + (levelCompleted * bonus);
+//			finalScore += 1000;
+//			System.out.println(finalScore);
+//		}
+//	}
+	
+	// Using return method
+	public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 		if(gameOver == true) {
 			int finalScore = score + (levelCompleted * bonus);
 			finalScore += 1000;
 			System.out.println(finalScore);
-		}
+			return finalScore;
+		} 
+		
+		return -1; // -1 can be used as err or value not found or invalid value
+		// else can be used as well
 	}
 }
