dfe30b0be7e8e6cc5d72e80e1bb16a1b427f87fe
Asif Hoque <asif.engr13@gmail.com>
2021-06-07 13:52:59 +0200

    JavaProgrammingMasterclass

--- src/operators/keywords/expression/methos/CodingChllnge.java
+++ src/operators/keywords/expression/methos/CodingChllnge.java
@@ -1,33 +1,35 @@
 package operators.keywords.expression.methos;
 
+import java.math.BigDecimal;
+
 public class CodingChllnge {
 	
 	public static void main(String[] srgs) {
-		checkNum(1);
-		checkNum(-1);
-		checkNum(0);
+//		checkNum(1);
+//		checkNum(-1);
+//		checkNum(0);
 		
 		//convert km into miles	using return method
-		long miles = toMilesPrHr(6.5);
-		System.out.println(miles);
+//		long miles = toMilesPrHr(6.5);
+//		System.out.println(miles);
 		
 		//convert km into miles	using no return
-		printCoversion(6.5);
+		//printCoversion(6.5);
 		
 		// kb to mb
-		printMegaBytesAndKiloBytes(2500);
-		printMegaBytesAndKiloBytes(-1024);
-		printMegaBytesAndKiloBytes(5000);
+//		printMegaBytesAndKiloBytes(2500);
+//		printMegaBytesAndKiloBytes(-1024);
+//		printMegaBytesAndKiloBytes(5000);
 		
 		// Barking dog
-		boolean res1 = shouldWakeUp(true, 1);
-		System.out.println(res1);
-		boolean res2 =shouldWakeUp(false, 2);
-		System.out.println(res2);
-		boolean res3 =shouldWakeUp(true, 8);
-		System.out.println(res3);
-		boolean res4 =shouldWakeUp(true, -1);
-		System.out.println(res4);
+//		boolean res1 = shouldWakeUp(true, 1);
+//		System.out.println(res1);
+//		boolean res2 =shouldWakeUp(false, 2);
+//		System.out.println(res2);
+//		boolean res3 =shouldWakeUp(true, 8);
+//		System.out.println(res3);
+//		boolean res4 =shouldWakeUp(true, -1);
+//		System.out.println(res4);
 		
 		// leap year
 //		isLeapYear(1700);
@@ -39,13 +41,31 @@ public class CodingChllnge {
 //		isLeapYear(2500);
 //		isLeapYear(2600);
 		
-		isLeapYear(1600);
-		isLeapYear(2000);
-		isLeapYear(2400);
+//		isLeapYear(1600);
+//		isLeapYear(2000);
+//		isLeapYear(2400);
+//		
+//		isLeapYear(-1600);
+//		isLeapYear(2017);
+//		isLeapYear(1924);
 		
-		isLeapYear(-1600);
-		isLeapYear(2017);
-		isLeapYear(1924);
+		// DecimalComp
+//		areEqualByThreeDecimalPlaces(-3.1756, -3.175);
+//		areEqualByThreeDecimalPlaces(3.175, 3.176);
+//		areEqualByThreeDecimalPlaces(3.0, 3.0);
+//		areEqualByThreeDecimalPlaces(-3.123, 3.123);
+		
+		//Equal sum checker
+//		hasEqualSum(1, 1, 1);
+//		hasEqualSum(1, 1, 2);
+//		hasEqualSum(1, -1, 0);
+		
+		//Teen num checker
+		hasTeen(9, 99, 19);
+		hasTeen(23, 15, 42);
+		hasTeen(22, 23, 34);
+		isTeen(9);
+		isTeen(13);
 
 	}
 	
@@ -122,4 +142,46 @@ public class CodingChllnge {
 		System.out.print("Incor input");
 		return false;	
 	}
+	
+	// Ch-6 DecimalComparator
+		public static boolean areEqualByThreeDecimalPlaces(double num1, double num2) {
+			// Check validity
+			if(Math.abs(num1 - num2) < 0.0009) {
+				System.out.print("True ");
+				return true;
+			}
+			System.out.print("False ");
+			return false;	
+		}
+		
+	// Ch-7 Equal sum checker
+		public static boolean hasEqualSum(int num1, int num2, int num3) {
+		// Check validity
+			if((num1 + num2) == num3) {
+				System.out.print("True ");
+				return true;
+			}
+			System.out.print("False ");
+			return false;	
+		}
+	// Ch-7 Equal sum checker
+		public static boolean hasTeen(int num1, int num2, int num3) {
+		// Check validity
+			if((num1 >= 13 && num1 <= 19) || (num2 >= 13 && num2 <= 19) || (num3 >= 13 && num3 <= 19)) {
+				System.out.print("True ");
+				return true;
+			}
+			System.out.print("False ");
+			return false;	
+		}
+		
+		public static boolean isTeen(int num1) {
+			// Check validity
+			if((num1 >= 13 && num1 <= 19)) {
+				System.out.print("True ");
+				return true;
+			}
+			System.out.print("False ");
+			return false;	
+		}
 }
