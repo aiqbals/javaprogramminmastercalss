e13cc59fd813823d3bc354706fabe02d0e7cca35
Asif Hoque <asif.engr13@gmail.com>
2021-06-09 17:04:48 +0200

    JavaProgrammingMasterclass

--- src/operators/keywords/expression/methos/MethodOverload.java
+++ src/operators/keywords/expression/methos/MethodOverload.java
@@ -2,10 +2,13 @@ package operators.keywords.expression.methos;
 
 public class MethodOverload {
 	public static void main(String[] args) {
-		calculateScore("Asif", 500);
-		calculateScore("A", 500, 5555);
-		calculateScore();
-		//System.out.println("Hello");
+//		calculateScore("Asif", 500);
+//		calculateScore("A", 500, 5555);
+//		calculateScore();
+//		//System.out.println("Hello");
+		
+		calcFtAndIncTom(6, 0);
+		calcFtAndIncTom(6, -1);
 	}
 	
 	public static int calculateScore(String playerName, int score) {
@@ -21,6 +24,29 @@ public class MethodOverload {
 	public static int calculateScore() {
 		System.out.print(" Player");
 		return 0;
+	}
+	
+	// Ch-1
+	public static double calcFtAndIncTom(double feet, double inches) {
+		if(feet >= 0 || ((inches >= 0) && (inches <= 12))){
+			double cm = (feet * 12) * 2.54;
+			cm += inches * 2.54;
+			System.out.print(feet + " feet, " + inches + " inches = " + cm + " cm");
+			return cm;
+		}
+		System.out.print(" Invalid ");
+		return -1;
+	}
+	
+	// Ch-1
+	public static double calcFtAndIncTom(double inches) {
+		if(inches < 0){
+			System.out.print(" Invalid ");
+			return -1;
+		}
+		double inch = (inches * 0.0833333);
+		
+		return inch;
 	}
 }
 
