b49eb28ecd1596f4a3fb7b745e70f1cd790485fc
Asif Hoque <asif.engr13@gmail.com>
2021-06-12 12:53:39 +0200

    JavaProgrammingMasterclass

--- src/controlflow/ForLoop.java
+++ src/controlflow/ForLoop.java
@@ -19,6 +19,38 @@ public class ForLoop {
 			System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10000.0, i)));
 			//to show 2dec point and avoid precision e.g, 7%, double interest 700.0000000000001.
 		}
+		
+		int count = 0;
+		for(int i=10; i<50; i++) {
+			
+			boolean prime = isPrime(i);
+			if(prime == true) {
+				count++;
+				System.out.println(i + " is prime no.");
+			}
+			if(count == 10) {
+				break;
+			}
+		}
+		
+	}
+	
+	public static boolean isPrime(int n) {
+		if(n == 1) {
+			System.out.println("Invalid input ");
+			return false;
+		}
+		//for(int i=2; i<=n/2; i++) {
+		for(int i=2; i <= (long) Math.sqrt(n); i++) {
+		// it is faster thn previous optimization
+			System.out.println("Looping " + i);
+			if(n % i == 0) {
+				//System.out.println("False");
+				return false;
+			}
+		}
+		//System.out.println("True");
+		return true;
 	}
 	
 	public static double calculateInterest(double amount, double interestRt) {
