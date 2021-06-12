16b63bf54b62a1d5b1945f880894cd8d4a581f0b
Asif Hoque <asif.engr13@gmail.com>
2021-06-12 10:13:19 +0200

    JavaProgrammingMasterclass

---
+++ src/controlflow/SwitchFlow.java
@@ -0,0 +1,16 @@
+package controlflow;
+
+public class SwitchFlow {
+	public static void main(String[] args) {
+		
+		String month = "July";
+		switch(month.toLowerCase()) {
+		case "Jan" : case "June" : case "July" :
+			System.out.println("This is month " + month);
+			break;
+		default: 
+			System.out.println("This is Unkown month ");
+			break;
+		}
+	}
+}
