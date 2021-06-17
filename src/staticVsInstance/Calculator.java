076a172f814eaf9b3dec7a38064925ea1e8d0daa
Asif Hoque <asif.engr13@gmail.com>
2021-06-17 19:50:34 +0200

    JavaProgrammingMasterclass

--- src/staticVsInstance/Calculator.java
+++ src/staticVsInstance/Calculator.java
@@ -1,8 +1,30 @@
 package staticVsInstance;
 
 public class Calculator {
+	private int a, b;
+	private static String name; // static variable is shared between instances 
+	private String name2;
 	
-	// Static method - independent method that do not use Instance variables, and methods
+	
+//	public Calculator() {
+//	}
+//	public Calculator(String name) {
+//		Calculator.name = name; // direct access to static variable
+//	}
+	
+	public Calculator(String name2) {
+		this.name2 = name2; // Using instance vaiable
+	}
+
+	public void printName() {
+		System.out.println(name);
+	}
+	public void printName2() {
+		System.out.println(name2);
+	}
+	
+	// When to create Static method - Is a independent method, when no use of Instance variables, and methods
+	// If there are use of instance methods or variables, it should probably be an instance method
 	public static void printSum(int a, int b) {
 		System.out.println(a+b);
 	}
@@ -10,7 +32,9 @@ public class Calculator {
 	// Instance method can access or use instance methods and variables
 	// Instance method can also access static methods and static variables directly 
 	
-	public void printSubstr(int a, int b) {
+	public void printSubstr(int n1, int n2) {
+		this.a = n1;
+		this.b = n2;
 		System.out.println(a-b);
-	}
+	}	
 }
--- src/staticVsInstance/Main.java
+++ src/staticVsInstance/Main.java
@@ -8,8 +8,21 @@ public class Main {
 		Main.printHello(); 
 		// Static method are called this way or only method name
 		
-		Calculator cal = new Calculator();
-		cal.printSubstr(5, 2); // Calculator instance need to be created to access printSubtr
+//		Calculator cal = new Calculator();
+//		cal.printSubstr(5, 2); // Calculator instance need to be created to access printSubtr
+		
+//		Calculator whatName = new Calculator("FirstNAme");
+//		Calculator whatName2 = new Calculator("LastName");
+//		whatName.printName(); //LastName
+//		whatName2.printName(); //LastName
+		// Both print Last name since Static variable is shared with two instance of Calculator
+		// Hence, variable name will be updated with second instance whatName2
+		
+		// Incase of instance vaiable
+		Calculator whatName = new Calculator("FirstNAme");
+		Calculator whatName2 = new Calculator("LastName");
+		whatName.printName2(); //FirstName
+		whatName2.printName2(); //LastName
 	}
 	
 	public static void printHello() {
